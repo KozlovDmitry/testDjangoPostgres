6217fed1a2d1f2e516d16a3a8a934ad8db5d3240
Kozlov Dmitriy <siderwinder@icloud.com>
2020-07-24 16:32:54 +0300

    Connect with Postgres. Change Requirements. Connectivity frontend

--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
 
-    return render(request, 'app1/index.html')
+    context = {
+        'letters': Test.objects.all()
+    }
+
+    return render(request, 'app1/index.html', context=context)
--- testPostgres/settings.py
+++ testPostgres/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'testPostgres.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '',
+        'NAME': 'smartjoe',
+        'PASSWORD': 'siderwinder',
+        'PORT': '5432',
+        'USER': 'smartjoe',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
